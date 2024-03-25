2963e7a92dd59c8be6c9004c7d6f1e2ddcba4d0b
Ramon Ledesma <rmon.led@gmail.com>
2024-03-24 18:28:43 -0700

    fix typos

--- src/main/java/com/team5430/util/log.java
+++ src/main/java/com/team5430/util/log.java
@@ -41,7 +41,7 @@ public class log{
                 }else if (errors.SupplyOverV == true){
                     println(motorName + ": voltage is above rated voltage.");
                 }else if (errors.SupplyUnstable == true){
-                    println(motorName + ": voltage is fluctuationg too much, check battery");
+                    println(motorName + ": voltage is fluctuating too much, check battery");
                 }else if (errors.UnderVoltage == true){
                     println(motorName + ": voltage is under 6.5V");
                 }else if (errors.HardwareESDReset == false && startup >= 1){
@@ -52,7 +52,7 @@ public class log{
     }
     public void motor(TalonFX name){
       String motorName = name.toString();
-      double startup = 1;
+      double startup = 0;
               if(name.getStickyFault_ProcTemp().getValue() == true){
             println(motorName + ": Temperature exceeded limit.");
                 }else if(name.getStickyFault_MissingDifferentialFX().getValue() == true){
@@ -68,7 +68,7 @@ public class log{
                 }else if (name.getStickyFault_OverSupplyV().getValue() == true){
                     println(motorName + ": voltage is above rated voltage.");
                 }else if (name.getStickyFault_UnstableSupplyV().getValue() == true){
-                    println(motorName + ": voltage is fluctuationg too much, check battery");
+                    println(motorName + ": voltage is fluctuating too much, check battery");
                 }else if (name.getStickyFault_Undervoltage().getValue() == true){
                     println(motorName + ": voltage is under 6.5V");
                 }else if (name.getStickyFault_BootDuringEnable().getValue() == false && startup >= 1){
